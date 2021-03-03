423fba633bcc9f049fe1e56e02cac5be1d394b62
yngtodd <young.todd.mk@gmail.com>
2021-03-03 00:42:00 -0500

    Reformat typescript

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { MikroORM } from "@mikro-orm/core";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-		dbName: 'reddit',
-		type: 'postgresql',
-		user: 'postgres',
-		password: 'postgres',
-		debug: process.env.NODE_ENV !== 'production',
-	});
+    const orm = await MikroORM.init({
+        dbName: 'reddit',
+        type: 'postgresql',
+        user: 'postgres',
+        password: 'postgres',
+        debug: process.env.NODE_ENV !== 'production',
+    });
 
-  console.log("Hello world!!");
+    console.log("Hello world!!");
 };
 
 main();
